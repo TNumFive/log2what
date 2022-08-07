326988f9e62fe320aa2c3ee6e64836d6b64111fd
TNumFive <fangxw@nuaa.edu.cn>
2022-08-07 23:05:50 +0800

    add log_list and buffer_size next: try std::move and re-struct the file_writer, db_writer is too slow

--- db_writer/db_writer.cpp
+++ db_writer/db_writer.cpp
@@ -25,6 +25,7 @@ inline void mkdir(string dir_path) {
 
 struct log {
     int64_t timestamp_nano;
+    level l;
     string moudle_name;
     string comment;
     string data;
@@ -44,14 +45,69 @@ struct db_info {
 static std::map<string, db_info> db_map;
 static std::mutex life_cycle_mutex;
 
+void db_writer::flush_log_list() {
+    using leve_type = std::underlying_type_t<level>;
+    int ret;
+    db_info &info = *(static_cast<db_info *>(db_info_ptr));
+    for (auto &&l : info.log_list) {
+        do {
+            string &module_name = l.moudle_name;
+            string &comment = l.comment;
+            string &data = l.data;
+            ret = sqlite3_bind_int64(info.stmt_ptr, 1, l.timestamp_nano);
+            if (ret != SQLITE_OK) {
+                info.logger_ptr->error("bind timestamp failed", sqlite3_errmsg(info.db_ptr));
+                break;
+            }
+            ret = sqlite3_bind_int64(info.stmt_ptr, 2, static_cast<leve_type>(l.l));
+            if (ret != SQLITE_OK) {
+                info.logger_ptr->error("bind level failed", sqlite3_errmsg(info.db_ptr));
+                break;
+            }
+            ret = sqlite3_bind_text(info.stmt_ptr, 3, module_name.c_str(), module_name.length() + 1, SQLITE_STATIC);
+            if (ret != SQLITE_OK) {
+                info.logger_ptr->error("bind module_name failed", sqlite3_errmsg(info.db_ptr));
+                break;
+            }
+            ret = sqlite3_bind_text(info.stmt_ptr, 4, comment.c_str(), comment.length() + 1, SQLITE_STATIC);
+            if (ret != SQLITE_OK) {
+                info.logger_ptr->error("bind comment failed", sqlite3_errmsg(info.db_ptr));
+                break;
+            }
+            ret = sqlite3_bind_text(info.stmt_ptr, 5, data.c_str(), data.length() + 1, SQLITE_STATIC);
+            if (ret != SQLITE_OK) {
+                info.logger_ptr->error("bind data failed", sqlite3_errmsg(info.db_ptr));
+                break;
+            }
+            ret = sqlite3_step(info.stmt_ptr);
+            if (ret != SQLITE_DONE) {
+                info.logger_ptr->error("step stmt failed", sqlite3_errmsg(info.db_ptr));
+                break;
+            }
+            ret = sqlite3_reset(info.stmt_ptr);
+            if (ret != SQLITE_OK) {
+                info.logger_ptr->error("reset stmt failed", sqlite3_errmsg(info.db_ptr));
+                break;
+            }
+        } while (false);
+        if (ret != SQLITE_OK && ret != SQLITE_DONE) {
+            std::stringstream ss;
+            ss << l.timestamp_nano << " " << to_string(l.l) << " " << l.comment << " " << l.data;
+            info.logger_ptr->error("insert log error", ss.str());
+        }
+    }
+    info.log_list.clear();
+}
+
 int prepare_table_and_stmt(db_info &info) {
     constexpr char create_table[] = "create table if not exists log("
                                     "timestamp int primary key not null,"
+                                    "level int,"
                                     "module_name text,"
                                     "comment text,"
                                     "data text"
                                     ");";
-    constexpr char insert[] = "insert into log values(?1,?2,?3,?4);";
+    constexpr char insert[] = "insert into log values(?1,?2,?3,?4,?5);";
     int ret;
     ret = sqlite3_exec(info.db_ptr, create_table, nullptr, nullptr, nullptr);
     if (ret != SQLITE_OK) {
@@ -64,7 +120,7 @@ int prepare_table_and_stmt(db_info &info) {
     return SQLITE_OK;
 }
 
-db_writer::db_writer(string url, writer *writer_ptr) {
+db_writer::db_writer(string url, size_t buffer_szie, writer *writer_ptr) {
     this->url = url;
     std::lock_guard<std::mutex> lock{life_cycle_mutex};
     auto &info = db_map[url];
@@ -86,6 +142,7 @@ db_writer::db_writer(string url, writer *writer_ptr) {
         }
         info.logger_ptr->info("open db", url);
     }
+    info.buffer_size = buffer_szie;
     db_info_ptr = static_cast<void *>(&info);
 }
 
@@ -94,6 +151,9 @@ db_writer::~db_writer() {
     auto &info = db_map[url];
     info.counter--;
     if (info.counter == 0) {
+        if (!info.log_list.empty()) {
+            flush_log_list();
+        }
         if (SQLITE_OK != sqlite3_finalize(info.stmt_ptr)) {
             info.logger_ptr->error("finalize stmt failed", sqlite3_errmsg(info.db_ptr));
         }
@@ -110,36 +170,13 @@ db_writer::~db_writer() {
 
 void db_writer::write(level l, string module_name, string comment, string data) {
     db_info &info = *(static_cast<db_info *>(db_info_ptr));
-    int ret;
     std::lock_guard<std::mutex> lock{info.db_mutex};
-    do {
-        ret = sqlite3_bind_int64(info.stmt_ptr, 1, get_nano_timestamp());
-        if (ret != SQLITE_OK) {
-            info.logger_ptr->error("bind ts failed", sqlite3_errmsg(info.db_ptr));
-            break;
-        }
-        ret = sqlite3_bind_text(info.stmt_ptr, 2, module_name.c_str(), module_name.length() + 1, SQLITE_STATIC);
-        if (ret != SQLITE_OK) {
-            info.logger_ptr->error("bind module_name failed", sqlite3_errmsg(info.db_ptr));
-            break;
-        }
-        ret = sqlite3_bind_text(info.stmt_ptr, 3, comment.c_str(), comment.length() + 1, SQLITE_STATIC);
-        if (ret != SQLITE_OK) {
-            info.logger_ptr->error("bind comment failed", sqlite3_errmsg(info.db_ptr));
-            break;
-        }
-        ret = sqlite3_bind_text(info.stmt_ptr, 4, data.c_str(), data.length() + 1, SQLITE_STATIC);
-        if (ret != SQLITE_OK) {
-            info.logger_ptr->error("bind data failed", sqlite3_errmsg(info.db_ptr));
-            break;
-        }
-        ret = sqlite3_step(info.stmt_ptr);
-        if (ret != SQLITE_DONE) {
-            info.logger_ptr->error("step stmt failed", sqlite3_errmsg(info.db_ptr));
-        }
-        ret = sqlite3_reset(info.stmt_ptr);
-        if (ret != SQLITE_OK) {
-            info.logger_ptr->error("reset stmt failed", sqlite3_errmsg(info.db_ptr));
-        }
-    } while (false);
+    log temp{.timestamp_nano = get_nano_timestamp(),
+             .l = l,
+             .comment = comment,
+             .data = data};
+    info.log_list.emplace_back(temp);
+    if (info.log_list.size() >= info.buffer_size) {
+        flush_log_list();
+    }
 }
--- db_writer/db_writer.hpp
+++ db_writer/db_writer.hpp
@@ -13,9 +13,10 @@ class db_writer : public writer {
   private:
     string url;
     void *db_info_ptr;
+    void flush_log_list();
 
   public:
-    db_writer(string url = "./log/log2.db", writer *writer_ptr = new writer);
+    db_writer(string url = "./log/log2.db", size_t buffer_szie = 100, writer *writer_ptr = new writer);
     ~db_writer() override;
     void write(level l, string module_name, string comment, string data) override;
 };
