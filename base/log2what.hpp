73d729b07937c1dea8fa71281bc133ad9eac743c
TNumFive <fangxw@nuaa.edu.cn>
2022-08-16 19:01:08 +0800

    add virtual to interface log2

--- base/log2what.hpp
+++ base/log2what.hpp
@@ -31,25 +31,25 @@ class log2 {
     log2(log2 &&other) = delete;
     log2 &operator=(const log2 &other) = delete;
     log2 &operator=(log2 &&other) = delete;
-    ~log2() {
+    virtual ~log2() {
         if (this->writer_ptr != nullptr) {
             delete this->writer_ptr;
             this->writer_ptr = nullptr;
         }
     }
-    void trace(const string &comment = "", const string &data = "") {
+    virtual void trace(const string &comment = "", const string &data = "") {
         writer_ptr->write(level::TRACE, module_name, comment, data);
     }
-    void debug(const string &comment = "", const string &data = "") {
+    virtual void debug(const string &comment = "", const string &data = "") {
         writer_ptr->write(level::DEBUG, module_name, comment, data);
     }
-    void info(const string &comment = "", const string &data = "") {
+    virtual void info(const string &comment = "", const string &data = "") {
         writer_ptr->write(level::INFO, module_name, comment, data);
     }
-    void warn(const string &comment = "", const string &data = "") {
+    virtual void warn(const string &comment = "", const string &data = "") {
         writer_ptr->write(level::WARN, module_name, comment, data);
     }
-    void error(const string &comment = "", const string &data = "") {
+    virtual void error(const string &comment = "", const string &data = "") {
         writer_ptr->write(level::ERROR, module_name, comment, data);
     }
 };
@@ -62,7 +62,7 @@ class log2lots {
   private:
     std::list<writer *> writer_list;
     string module_name;
-    void write(const level l, const string &comment, const string &data) {
+    virtual void write(const level l, const string &comment, const string &data) {
         for (auto &&i : writer_list) {
             i->write(l, module_name, comment, data);
         }
@@ -70,33 +70,33 @@ class log2lots {
 
   public:
     log2lots(const string &module_name = "root") : module_name{module_name} {};
-    ~log2lots() {
+    log2lots(const log2lots &other) = delete;
+    log2lots(log2lots &&other) = delete;
+    log2lots &operator=(const log2lots &other) = delete;
+    log2lots &operator=(log2lots &&other) = delete;
+    virtual ~log2lots() {
         while (!writer_list.empty()) {
             delete writer_list.front();
             writer_list.pop_front();
         }
     }
-    log2lots(const log2lots &other) = delete;
-    log2lots(log2lots &&other) = delete;
-    log2lots &operator=(const log2lots &other) = delete;
-    log2lots &operator=(log2lots &&other) = delete;
-    log2lots &append_writer(writer *w) {
+    virtual log2lots &append_writer(writer *w) {
         this->writer_list.push_back(w);
         return *this;
     }
-    void trace(const string &comment = "", const string &data = "") {
+    virtual void trace(const string &comment = "", const string &data = "") {
         write(level::TRACE, comment, data);
     }
-    void debug(const string &comment = "", const string &data = "") {
+    virtual void debug(const string &comment = "", const string &data = "") {
         write(level::DEBUG, comment, data);
     }
-    void info(const string &comment = "", const string &data = "") {
+    virtual void info(const string &comment = "", const string &data = "") {
         write(level::INFO, comment, data);
     }
-    void warn(const string &comment = "", const string &data = "") {
+    virtual void warn(const string &comment = "", const string &data = "") {
         write(level::WARN, comment, data);
     }
-    void error(const string &comment = "", const string &data = "") {
+    virtual void error(const string &comment = "", const string &data = "") {
         write(level::ERROR, comment, data);
     }
 };
